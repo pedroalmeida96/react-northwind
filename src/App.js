2b50ed8ac85aaf7131f966a99cc7fceb8c6f0f59
pedroalmeida96 <pedro.gregorio96@gmail.com>
2022-01-18 14:19:34 +0000

    first line

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <p>My first line of react code!</p>
+      </header>
     </div>
   );
 }
