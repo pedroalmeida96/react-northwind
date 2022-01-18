78b669adcaf657a298fb54907e9f29cb13fbd5e7
pedroalmeida96 <pedro.gregorio96@gmail.com>
2022-01-18 14:53:03 +0000

    add + list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './Header';
+import Header from './components/Header';
+import CategoriesList from './components/CategoriesList';
+import AddCategory from './components/AddCategory';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Header />
+        <div className="main-content">
+          <Routes>
+            <Route component={CategoriesList} path="/" />
+            <Route component={AddCategory} path="/add" />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/CategoriesList.js
@@ -0,0 +1,5 @@
+const CategoriesList = () => {
+
+}
+
+export default CategoriesList;
